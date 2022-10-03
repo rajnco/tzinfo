23f867104ccf589c22d28d582171fcf7436c3e02
Rajkumar N <rajkumar.natarajan@gmail.com>
2022-10-03 10:38:58 +0530

    formatted with black

--- tests/test_tzinfo.py
+++ tests/test_tzinfo.py
@@ -1,22 +1,26 @@
 from tzinfo import tzinfo
 
+
 def test_offset():
     ttzi = tzinfo.TZInfo()
     ttzi.offset(5.5)
     assert len(ttzi.matched) == 2
 
+
 def test_match():
     ttzi = tzinfo.TZInfo()
-    ttzi.match('Calcutta')
-    assert len(ttzi.matched) == 1 
+    ttzi.match("Calcutta")
+    assert len(ttzi.matched) == 1
 
-def test_match_offset_both(): 
+
+def test_match_offset_both():
     ttzi = tzinfo.TZInfo()
-    ttzi.match('Calcutta')
+    ttzi.match("Calcutta")
     ttzi.offset(5.5)
     assert len(ttzi.matched) == 2
-    
-def test_offset_absolute_value(): 
+
+
+def test_offset_absolute_value():
     ttzi = tzinfo.TZInfo()
     ttzi.offset(5.5)
 
@@ -24,5 +28,3 @@ def test_offset_absolute_value():
     ttzi2.offset(-5.5)
 
     assert ttzi.matched == ttzi2.matched
-
-    
--- tests/test_tzinfo_2nd.py
+++ tests/test_tzinfo_2nd.py
@@ -2,7 +2,6 @@ from tzinfo import tzinfo
 from unittest import TestCase
 
 
-
 class TestTZInfo(TestCase):
     def setUp(self):
         self.ttzi = tzinfo.TZInfo()
@@ -12,15 +11,15 @@ class TestTZInfo(TestCase):
         assert len(self.ttzi.matched) == 2
 
     def test_match(self):
-        self.ttzi.match('Calcutta')
-        assert len(self.ttzi.matched) == 1 
+        self.ttzi.match("Calcutta")
+        assert len(self.ttzi.matched) == 1
 
-    def test_match_offset_both(self): 
-        self.ttzi.match('Calcutta')
+    def test_match_offset_both(self):
+        self.ttzi.match("Calcutta")
         self.ttzi.offset(5.5)
         assert len(self.ttzi.matched) == 2
-    
-    def test_offset_absolute_value(self): 
+
+    def test_offset_absolute_value(self):
         self.ttzi.offset(5.5)
 
         ttzi2 = tzinfo.TZInfo()
